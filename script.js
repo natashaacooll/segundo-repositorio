11a2be053a9436c48c169285ed9ce1ecf4657d7a
natashadacooll <144967473+natashaacooll@users.noreply.github.com>
2025-04-07 22:53:23 -0300

    aula 07/04

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentarfonte');
+    aumentaFonteBotao.addEventListener('click', function(){ })
+  
+    })
+    const botaoDeAcessibilidade = document.getElementById
+    ('botao-acebilidade');
+    const opcoesDeAcessibilidade = document.getElementById
+    ('opcoes-acessibilidades');
+
+   })
